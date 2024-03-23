89558a034f1c0f43f2d959f6837aa66f1a5671ee
Jackson Arnold <jacksonarnold96@gmail.com>
2024-03-22 21:01:52 -0500

    edit scripts to only remove names from PDFs

--- script.py
+++ script.py
@@ -4,7 +4,24 @@ from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
 
 
-def anonymize_pdf(pdf_path):
+def anonymize_document(pdf_path, isPdf=True):
+    textstring = ""
+
+    if isPdf:
+        textstring = read_pdf(pdf_path)
+    else:
+        textstring = read_text(pdf_path)
+
+    print(textstring)
+
+    print("=====================================")
+    print("Anonymizing sensitive data in the PDF")
+    print("=====================================\n\n")
+
+    return anonymize_text(textstring)
+
+
+def read_pdf(file_path):
     text = textract.process(pdf_path, method="pdfminer")
 
     # Remove null characters
@@ -15,28 +32,32 @@ def anonymize_pdf(pdf_path):
     # Remove non-printable characters
     textstring = re.sub(r"[^\x20-\x7e]", "", textstring)
 
-    print(textstring)
+    return textstring
 
-    print("=====================================")
-    print("Anonymizing sensitive data in the PDF")
-    print("=====================================\n\n")
 
+def read_text(file_path):
+    with open(file_path, "r") as file:
+        text = file.read()
+    return text
+
+
+def anonymize_text(text):
     # Initialize Presidio analyzer and anonymizer
     analyzer = AnalyzerEngine()
     anonymizer = AnonymizerEngine()
 
     # Analyze the text to identify sensitive data
-    analyzer_results = analyzer.analyze(text=textstring, language="en")
+    analyzer_results = analyzer.analyze(text=text, entities=["PERSON"], language="en")
 
     # Anonymize the sensitive data
-    anonymized_text = anonymizer.anonymize(
-        text=textstring, analyzer_results=analyzer_results
-    )
+    anonymized_text = anonymizer.anonymize(text=text, analyzer_results=analyzer_results)
 
     return anonymized_text
 
 
 # Example usage
 pdf_path = "PARTNERSHIP_AGREEMENT.pdf"
-anonymized_text = anonymize_pdf(pdf_path)
+txt_path = "NonCompeteText.txt"
+# anonymized_text = anonymize_document(pdf_path)
+anonymized_text = anonymize_document(txt_path, False)
 print(anonymized_text)
